42bba20d58e191056d87416f808a4285f873824a
danugans <mas.danu29@gmail.com>
2022-04-29 13:44:48 +0700

    draft

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
