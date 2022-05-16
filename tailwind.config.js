19ace50eeaa431eab5520255cd0f4b63e80b0469
danugans <mas.danu29@gmail.com>
2022-05-16 16:33:47 +0700

    beta

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   content: ['index.html', './src/**/*.{html,js,tsx}', 'admin-page/**/*.{html,js,tsx}', 'xflash-web/**/*.{html,js,tsx}'],
   purge: ['index.html', './src/**/*.{html,js,tsx}', 'admin-page/**/*.{html,js,tsx}', 'xflash-web/**/*.{html,js,tsx}'],
-  enabled: process.env.NODE_ENV === 'production',
   theme: {
     container: {
       center: true,
